90c1fd6aa688e24ce560081942c9ac2bee87e8fb
kentos <kent@kentcederstrom.se>
2019-08-16 22:25:16 +0200

    v1.0

---
+++ README.md
@@ -0,0 +1,25 @@
+# Move selection to new file
+
+The refactorers best friend!<br>
+Select your code,<br>
+Cmd+P,<br>
+'Move selection to new file',<br>
+💥
+
+## Features
+
+This extension takes the text in selection and moves it to a new file. This comes in handy when for instance refactoring large files and you want to move code fast.
+It basically removes the lines you've selected and moves it.
+
+## Release Notes
+
+We try to keep release notes.
+
+### 1.0.0
+
+Initial release of Move selection to new file.
+
+---
+
+
+**Enjoy!**
---
+++ extension.js
@@ -0,0 +1,53 @@
+const vscode = require('vscode');
+
+function moveToNewDoc(codeToMove) {
+	vscode.workspace.openTextDocument().then(doc => {
+		vscode.window.showTextDocument(doc, 1, false).then(editor => {
+			editor.edit(edit => {
+				edit.insert(new vscode.Position(0, 0), codeToMove)
+			})
+		})
+	})
+}
+
+function activate(context) {
+	console.log('Extension "move-selection-to-new-file" is now active!');
+
+	function moveSelectionToNewFile() {
+		const editor = vscode.window.activeTextEditor;
+		const selection = editor.selection
+		const codeToMove = editor.document.getText(editor.selection)
+
+		const removeLines = [selection.start.line, selection.end.line]
+
+		if (selection.start.line > 0) {
+			const beforeLine = editor.document.lineAt(selection.start.line - 1)
+			if (beforeLine.isEmptyOrWhitespace) {
+				removeLines.unshift(beforeLine.lineNumber)
+			}
+		}
+
+		editor.edit(edit => {
+			const first = removeLines.shift()
+			const last = removeLines.pop()
+			Array.from(
+				{ length: (last + 1) - first },
+				(_v, k) => k + first
+			).forEach((lineNumber) => {
+				const line = editor.document.lineAt(lineNumber)
+				edit.delete(line.rangeIncludingLineBreak)
+			})
+		})
+		moveToNewDoc(codeToMove)
+	}
+
+	context.subscriptions.push(vscode.commands.registerCommand('extension.moveSelectionToNewFile', moveSelectionToNewFile));
+}
+
+exports.activate = activate;
+
+function deactivate() {
+	vscode.window.showInformationMessage('Goodbye!')
+}
+
+module.exports = { activate, deactivate }
